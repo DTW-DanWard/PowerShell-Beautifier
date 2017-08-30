e797340448b19a3ad262af62fe11d8160347cfbe
Dan Ward <dtward@gmail.com>
2017-08-30 14:35:26 -0400

    cleanup continues, not done

--- test/Automation/Invoke-RunTestScriptInDockerCoreContainers.ps1
+++ test/Automation/Invoke-RunTestScriptInDockerCoreContainers.ps1
@@ -3,16 +3,21 @@
 # Params for script
 # Function for writing error info; params $Command $ErrorInfo
 # Move everything to functions
-# asdf error handling here in function Get-DockerContainerStatusInfo, more
+
 # Move get container names to function
 # Output parameter / run info at top of script
 # Add parameter for Quiet option, returns $true or $false
 #   batch info to output in case of error?
 # Validate params for stuff to copy, stuff to run
 # Clean up all asdf
-# Params for exiting testing if one container fails
-# Go through TechTasks notes for anything else missing
+# Note in main about call function, if error exits
 # Add readme.md to Automation folder
+# run script through beautifier
+
+# No? Params for exiting testing if one container fails (assume always test all?)
+#   Or exit after first failure?  Probably exit after first
+# Go through TechTasks notes for anything else missing
+
 
 # in help describe path building of path for test folder path, test file path and option
 
@@ -23,76 +28,214 @@
 [string]$ContainerTestFilePath = "PowerShell-Beautifier/test/Invoke-DTWBeautifyScriptTests.ps1 -Quiet"
 
 
-# asdf need error handling in function
-function Get-DockerContainerStatusInfo {
-  $Pattern = "([^`t]+)`t([^`t]+)`t([^`t]+)`t([^`t]+)"
-  $ContainerInfo = docker ps -a --format "{{.ID}}`t{{.Names}}`t{{.Image}}`t{{.Status}}" | ForEach-Object {
-    $Match = Select-String -InputObject $_ -Pattern $Pattern
-    New-Object PSObject -Property ([ordered]@{
-        ContainerId = $Match.Matches.Groups[1].Value
-        Name        = $Match.Matches.Groups[2].Value
-        Image       = $Match.Matches.Groups[3].Value
-        Status      = $Match.Matches.Groups[4].Value
-      })
+#region Misc functions
+
+#region Function: Out-ErrorInfo
+<#
+.SYNOPSIS
+Write-Output error information when running a command
+.DESCRIPTION
+Write-Output error information when running a command
+.PARAMETER Command
+Command that was run
+.PARAMETER Parameters
+Parameters for the command
+.PARAMETER ErrorInfo
+Error information captured to display
+.EXAMPLE
+Out-ErrorInfo -Command "docker" -Parameters "--notaparam" -ErrorInfo $CapturedError
+# Writes command, parameters and error info to output
+#>
+function Out-ErrorInfo {
+  #region Function parameters
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Command,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [object[]]$Parameters,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [object]$ErrorInfo
+  )
+  #endregion
+  process {
+    Write-Output "Error occurred running this command:"
+    Write-Output "  $Cmd $Params"
+    Write-Output "Error is: $ErrorInfo"
   }
-  $ContainerInfo
-}
-
-
-
-
-
-#region Validate $DockerHubRepository - one slash surrounded by text
-# the value for $DockerHubRepository should be: <team name>/<project name>
-# i.e. it should have only 1 slash in it 'in the middle' of other characters
-if ($DockerHubRepository -notmatch '^[^/]+/[^/]+$') {
-  Write-Output "The format for DockerHubRepository is incorrect: $DockerHubRepository"
-  Write-Output "It should be in the format: TeamName/ProjectName"
-  Write-Output "i.e. it should have only 1 slash, surrounded by other text."
-  exit
 }
 #endregion
 
 
-#region Check if Docker installed
-# different types of errors depending on docker not being found or --version not
-# working; run in try/catch and capture error stream
-$Results = $null
-try {
-  $Cmd = "docker"
-  $Params = "--version"
-  $Results = & $Cmd $Params 2>&1
-  if ($? -eq $false -or $LastExitCode -ne 0) {
-    Write-Output "Docker does not appear to be installed or working correctly: $Results"
-    exit
+#region Functions: Confirm-DockerHubRepositoryFormatCorrect
+<#
+.SYNOPSIS
+Confirms script param $DockerHubRepository is <team name>/<project name>
+.DESCRIPTION
+Confirms script param $DockerHubRepository is <team name>/<project name>, 
+i.e. it should have only 1 slash in it 'in the middle' of other characters.
+If correct, does nothing, if incorrect writes info and exits script.
+#>
+function Confirm-DockerHubRepositoryFormatCorrect {
+  process {
+    # the value for $DockerHubRepository should be: <team name>/<project name>
+    # i.e. it should have only 1 slash in it 'in the middle' of other characters
+    if ($DockerHubRepository -notmatch '^[^/]+/[^/]+$') {
+      Write-Output "The format for DockerHubRepository is incorrect: $DockerHubRepository"
+      Write-Output "It should be in the format: TeamName/ProjectName"
+      Write-Output "i.e. it should have only 1 slash, surrounded by other text."
+      exit
+    }
   }
 }
-catch {
-  Write-Output "Docker does not appear to be installed or working correctly: $_"
-  exit
+#endregion
+
+
+#region Functions: Get-DockerHubProjectTagInfo
+<#
+.SYNOPSIS
+Returns Docker hub project tag info for $DockerHubRepository
+.DESCRIPTION
+Returns Docker hub project tag info for $DockerHubRepository; format is PSObjects.
+#>
+function Get-DockerHubProjectTagInfo {
+  process {
+    # path to tags for Docker project
+    $ImageTagsUri = "https://hub.docker.com/v2/repositories/" + $DockerHubRepository + "/tags"
+    try {
+      $Response = Invoke-WebRequest -Uri $ImageTagsUri
+      # Convert JSON response to PSObjects and return
+      (ConvertFrom-Json -InputObject $Response.Content).results
+    } catch {
+      Write-Output "Error occurred calling Docker hub project tags url"
+      Write-Output "  Url:   $ImageTagsUri"
+      Write-Output "  Error: $($_.Exception.Message)"
+      exit
+    }
+  }
 }
 #endregion
+
+
+#endregion
+
+
+
+#region All Docker command functions
+
+#region Function: Confirm-DockerInstalled
+<#
+.SYNOPSIS
+Confirms docker is installed
+.DESCRIPTION
+Confirms docker is installed; if installed ('docker --version' works) then function
+does nothing.  If not installed, reports error and exits script.
+#>
+function Confirm-DockerInstalled {
+  process {
+    try {
+      $Cmd = "docker"
+      $Params = @("--version")
+      $Results = & $Cmd $Params 2>&1
+      if ($? -eq $false -or $LastExitCode -ne 0) {
+        Write-Output "Docker does not appear to be installed or working correctly."
+        Out-ErrorInfo -Command $Cmd -Parameters $Params -ErrorInfo $Results
+        exit
+      }
+    }
+    catch {
+      Write-Output "Docker does not appear to be installed or working correctly."
+      Out-ErrorInfo -Command $Cmd -Parameters $Params -ErrorInfo $_.Exception.Message
+      exit
+    }
+  }
+}
+#endregion
+
+
+
+
+
+
+#region Function: Get-DockerContainerStatus
+<#
+.SYNOPSIS
+Returns docker container info as PSObjects
+.DESCRIPTION
+Returns docker container info (ps -a) as PSObjects with these properties:
+ContainerId, Name, Image, Status
+If error occurs, reports error and exits script.
+.EXAMPLE
+Get-DockerContainerStatus
+ContainerId  Name        Image                            Status
+-----------  ----        -----                            ------
+1c0fc1715cd8 ubuntu16.04 microsoft/powershell:ubuntu16.04 Exited (0) 17 minutes ago
+422b3e0d337a test6       microsoft/powershell:ubuntu16.04 Exited (0) 5 days ago
+#>
+function Get-DockerContainerStatus {
+  process {
+    # regex to extract 4 items from docker format output
+    $Pattern = "([^`t]+)`t([^`t]+)`t([^`t]+)`t([^`t]+)"
+    $ContainerInfo = $null
+    try {
+      $Cmd = "docker"
+      $Params = @("ps", "-a", "--format", "{{.ID}}`t{{.Names}}`t{{.Image}}`t{{.Status}}")
+      $Results = & $Cmd $Params 2>&1
+      if ($? -eq $false -or $LastExitCode -ne 0) {
+        Out-ErrorInfo -Command $Cmd -Parameters $Params -ErrorInfo $Results
+        exit
+      }
+      # now parse results to get individual properties
+      $ContainerInfo = $Results | ForEach-Object {
+        $Match = Select-String -InputObject $_ -Pattern $Pattern
+        New-Object PSObject -Property ([ordered]@{
+            ContainerId = $Match.Matches.Groups[1].Value
+            Name        = $Match.Matches.Groups[2].Value
+            Image       = $Match.Matches.Groups[3].Value
+            Status      = $Match.Matches.Groups[4].Value
+          })
+        }
+      $ContainerInfo
+    }
+    catch {
+      Out-ErrorInfo -Command $Cmd -Parameters $Params -ErrorInfo $_.Exception.Message
+      exit
+    }
+  }
+}
+#endregion
+
+
+
+
+
+
+
+
+#endregion
+
+
+
+# Programming note: to improve simplicity and readability, if any of the below functions
+# generates an error, info is written and the script is exited from within the function.
+
+Confirm-DockerInstalled
+
+# confirm $DockerHubRepository is <team name>/<project name>
+Confirm-DockerHubRepositoryFormatCorrect
+
+# get Docker image names and other details from Docker hub project tags (format PSObjects)
+$ImageTagsDataContent = Get-DockerHubProjectTagInfo
 
 
 #region Get Docker hub project tag info and store in new hashtable $ImageTagsData
 
-# path to tags for Docker project
-$ImageTagsUri = "https://hub.docker.com/v2/repositories/" + $DockerHubRepository + "/tags"
 
-#region Confirm url is valid and get image tags data content
-try {
-  $WebRequest = Invoke-WebRequest -Uri $ImageTagsUri
-}
-catch {
-  Write-Output "Docker hub project tags url failed."
-  Write-Output "Url is:  $ImageTagsUri"
-  Write-Output "Error:   $($_.Exception.Message)"
-  exit
-}
-#endregion
 
-# Convert $ImageTagsDataContent PSObject[] to hashtable of hashtables
-$ImageTagsDataContent = (ConvertFrom-Json -InputObject $WebRequest.Content).results
+
 # for each tag, create an entry in hash table $ImageTagsData
 #   the key will be the tag name
 #   the value will be a new hashtable containing the data from the PSObject plus
@@ -200,8 +343,8 @@ if ($ValidTestImageTagNames -eq $null) {
 
 
 
-
-$LocalContainerStatusInfo = Get-DockerContainerStatusInfo
+Write-Output "Getting container status"
+$LocalContainerStatusInfo = Get-DockerContainerStatus
 
 
 
@@ -243,7 +386,7 @@ $ValidTestImageTagNames | ForEach-Object {
       # wait a second then update local container status info
 # asdf
       # Start-Sleep -Seconds 1
-      $LocalContainerStatusInfo = Get-DockerContainerStatusInfo
+      $LocalContainerStatusInfo = Get-DockerContainerStatus
     }
     catch {
       $ErrorInfo = $_
@@ -274,7 +417,7 @@ $ValidTestImageTagNames | ForEach-Object {
         # wait a second then update local container status info
 # asdf
 #        Start-Sleep -Seconds 1
-        $LocalContainerStatusInfo = Get-DockerContainerStatusInfo
+        $LocalContainerStatusInfo = Get-DockerContainerStatus
       }
       catch {
         $ErrorInfo = $_
@@ -369,7 +512,7 @@ $ValidTestImageTagNames | ForEach-Object {
     # wait a second then update local container status info
 # asdf
     #    Start-Sleep -Seconds 1
-    $LocalContainerStatusInfo = Get-DockerContainerStatusInfo
+    $LocalContainerStatusInfo = Get-DockerContainerStatus
   }
   catch {
     $ErrorInfo = $_
